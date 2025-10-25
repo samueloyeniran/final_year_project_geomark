76726845b7b6452ed39b66c21d51434052b5d196
samuel <soyeniran200@gmail.com>
2025-10-24 21:57:47 -0700

    feat(firebase): integrate Firebase with Flutter, add configuration and necessary plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 //import 'package:geomark/logics/login_logic.dart';
 //import 'package:geomark/splashscreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geomark/components/SignInBox.dart';
+import 'package:geomark/firebase_options.dart';
 import 'package:geomark/pages/Attendance_history.dart';
 import 'package:geomark/pages/Auth/Signinpage.dart';
 import 'package:geomark/pages/Auth/forgettenPasswordPage.dart';
@@ -15,13 +17,8 @@ import 'package:go_router/go_router.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 Future<void> main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-
-  // await Supabase.initialize(
-  //   url: 'https://jrlwvdcplzkcgsmrusfm.supabase.co',
-  //   anonKey:
-  //       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpybHd2ZGNwbHprY2dzbXJ1c2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5MjExNjAsImV4cCI6MjA3NDQ5NzE2MH0.EAd3SXyWUDcX03jN-7iofEzZqO5_BcqbsNaBUvDGwvs',
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
